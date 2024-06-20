94be10f99819a101915c98376985c1d387550d13
GiovannyOtarola <giovannyxdd@gmail.com>
2024-06-20 14:34:06 -0400

    creando registro

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID,OnInit  } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
+import { Router } from '@angular/router'; 
+
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule,ReactiveFormsModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.scss'
 })
-export class RegistroComponent {
+export class RegistroComponent implements OnInit {
+  registroForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.registroForm = this.fb.group({
+      nombre_completo: ['', Validators.required],
+      usuario: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      fecha_nacimiento: ['', [Validators.required, this.edadMinimaValidator(13)]],
+      direccion_despacho: ['', Validators.required],
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[0-9])(?=.*[A-Z]).{6,18}$/)]],
+      confirm_password: ['', Validators.required]
+    }, { validators: this.passwordMatchValidator });
+  }
+
+  ngOnInit(): void { }
+
+  onSubmit(): void {
+    if (this.registroForm.valid) {
+      const userData = this.registroForm.value;
+      //guardar datos en localStorage
+      localStorage.setItem('userData', JSON.stringify(userData));
+      console.log('Datos guardados:', userData);
+      this.router.navigate(['/login']);
+    } else {
+      // Mostrar mensajes de error
+      this.registroForm.markAllAsTouched();
+    }
+  }
+
+  passwordMatchValidator(group: AbstractControl): ValidationErrors | null {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirm_password')?.value;
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
+
+  edadMinimaValidator(edadMinima: number) {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (!control.value) return null;
+      const today = new Date();
+      const birthDate = new Date(control.value);
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+      return age < edadMinima ? { edadMinima: true } : null;
+    };
+  }
 }
