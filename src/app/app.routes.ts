6c55cfa5b34421606f347d0529f132d86b43a715
GiovannyOtarola <giovannyxdd@gmail.com>
2024-06-20 14:32:20 -0400

    configurando rutas y login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
-import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { IndexComponent } from './components/index/index.component';
+import { CarritoComponent } from './components/carrito/carrito.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { RecuperacionComponent } from './components/recuperacion/recuperacion.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    //esto indica cual es la primera pagina que se debe inicar junto con la app
+    {path: '', redirectTo: 'index', pathMatch: 'full'},
+    {path: 'login', component: LoginComponent},
+    {path: 'registro', component: RegistroComponent},
+    {path: 'index', component: IndexComponent},
+    {path: 'carrito', component: CarritoComponent},
+    {path: 'admin', component: AdminComponent},
+    {path: 'perfil', component: PerfilComponent},
+    {path: 'recuperacion', component: RecuperacionComponent}
+
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
+  export class RegistroModule { }
+
+ 
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Renderer2, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent{
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+  
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const formData = this.loginForm.value;
+      const storedUserData = localStorage.getItem('userData');
+      if (storedUserData) {
+        const parsedUserData = JSON.parse(storedUserData);
+
+        if (formData.email === parsedUserData.email && formData.password === parsedUserData.password) {
+          console.log('Inicio de sesión exitoso');
+          alert('Inicio de sesión exitoso');
+          //almacenar estado de sesion 
+          localStorage.setItem('sessionActive', 'true');
+          localStorage.setItem('loggedInUser', JSON.stringify({ email: formData.email, nombre_completo: parsedUserData.nombre_completo }));
+         
+          
+           // Redirigir al usuario a la página principal después del inicio de sesión exitoso
+           this.router.navigate(['/principal']).then(() => {
+            // Recargar la página después de redirigir
+            window.location.reload();
+          });
+        } else {
+          console.error('Correo o contraseña incorrectos');
+          alert('Correo o contraseña incorrectos');
+        }
+      } else {
+        console.error('No se encontraron datos de usuario almacenados');
+        alert('No se encontraron datos de usuario almacenados');
+      }
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
 
 }
