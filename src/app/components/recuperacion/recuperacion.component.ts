2f9f867547ef80e656a0b8a405bc9f1627c76909
Giovanny Otarola <giovannyxdd@gmail.com>
2024-06-21 13:49:17 -0400

    agregando funcionalidad al componente recuperacion

--- src/app/components/recuperacion/recuperacion.component.ts
+++ src/app/components/recuperacion/recuperacion.component.ts
@@ -10,4 +10,26 @@ import { RouterModule } from '@angular/router';
 })
 export class RecuperacionComponent {
 
+  constructor(){}
+
+  mostrarPassword(): void {
+    const email = (document.getElementById('usuarioEmail') as HTMLInputElement).value;
+    const storedUserList = localStorage.getItem('userList');
+
+    if(storedUserList){
+      const usuarios = JSON.parse(storedUserList);
+
+      //Buscar usuario por Email
+      const usuario = usuarios.find((user: any) => user.email === email);
+      if (usuario) {
+        // Mostrar la contraseña en una alerta
+        alert(`La contraseña del usuario ${usuario.email} es: ${usuario.password}`);
+      } else {
+        alert('No se encontró ningún usuario con el correo electrónico proporcionado.');
+      }
+    } else {
+      alert('No hay usuarios almacenados.');
+    }
+  }
+
 }
