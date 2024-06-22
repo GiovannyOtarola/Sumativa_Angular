c18368621cfe819f8def8963975aff532f62d660
GiovannyOtarola <giovannyxdd@gmail.com>
2024-06-22 16:52:06 -0400

    documentando servicio de autenticacion

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
+/**
+ * @description
+ * Servicio de autenticacion que maneja el estado de autenticacion del usuario.
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private authState = new BehaviorSubject<boolean>(this.isAuthenticated());
 
+  /**
+   * Inicializa el estado de autenticacion del servicio.
+   */
   constructor() {
     this.authState.next(this.isAuthenticated()); // Inicializar el estado al inicio
   }
 
+  /**
+   * Verifica si el usuario esta autenticado
+   * 
+   * @returns {boolean} -Devuelve 'true' si el usuario esta autenticado y en caso contrario 'false'.
+   */
   isAuthenticated(): boolean {
     if (typeof localStorage !== 'undefined') {
       return localStorage.getItem('sessionActive') === 'true';
@@ -18,12 +30,20 @@ export class AuthService {
     return false;
   }
 
+  /**
+   * Obtiene un observable que notifica el estado de autenticación.
+   * 
+   * @returns {Observable<boolean>} - Un observable que emite el estado de autenticación.
+   */
   getAuthState() {
     return this.authState.asObservable();
   }
 
-  
-
+  /**
+   * Cierra la sesion del usuario.
+   * 
+   * Elimina los datos de autenticación del almacenamiento local y actualiza el estado de autenticación.
+   */
   logout(): void {
     if (typeof localStorage !== 'undefined') {
       localStorage.removeItem('sessionActive');
