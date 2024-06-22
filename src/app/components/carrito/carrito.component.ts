575edea37a3b68d924b8ae445209582c1a3516b9
GiovannyOtarola <giovannyxdd@gmail.com>
2024-06-22 16:33:27 -0400

    documentando componente carrito

--- src/app/components/carrito/carrito.component.ts
+++ src/app/components/carrito/carrito.component.ts
@@ -4,6 +4,12 @@ import { RouterModule } from '@angular/router';
 import { Juego } from '../index/index.component';
 import { AuthService } from '../services/services.component';
 
+/**
+ * @description
+ * Componente que maneja el carrito de compras.
+ * 
+ * Este componente permite ver, agregar, eliminar, vaciar los juegos en el carrito y realizar la compra.
+ */
 @Component({
   selector: 'app-carrito',
   standalone: true,
@@ -18,11 +24,18 @@ export class CarritoComponent {
 
   constructor(private authService: AuthService) { }
 
+  /**
+   * Inicializa el componente, estableciendo el estado de autenticacion y cargando el carrito desde el localStorage
+   */
   ngOnInit(): void {
     this.isLoggedIn = this.authService.isAuthenticated();
     this.cargarCarritoDesdeLocalStorage();
   }
 
+  /**
+   * Elimina un juego del carrito
+   * @param {Juego} juego -El juego que sera eliminado del carrito.
+   */
   eliminarDelCarrito(juego: Juego): void {
     const index = this.juegosEnCarrito.findIndex(j => j.id === juego.id);
     if (index !== -1) {
@@ -31,21 +44,34 @@ export class CarritoComponent {
     }
   }
 
+  /**
+   * Vacia todo el carrito.
+   */
   vaciarCarrito(): void {
     this.juegosEnCarrito = [];
     this.guardarCarritoEnLocalStorage();
   }
 
+  /**
+   * Calcula el total a pagar por los juegos en el carrito
+   * 
+   * @returns {number} - El total a pagar
+   */
   calcularTotal(): number {
     return this.juegosEnCarrito.reduce((acc, juego) => acc + (juego.cantidad || 0) * juego.precio, 0);
   }
 
-  comprar(): void {
-    
+  /**
+   * Realiza la compra de los juegos en el carrito y vacia el carrito.
+   */
+  comprar(): void {    
     alert('¡Compra realizada con éxito!');
     this.vaciarCarrito();
   }
 
+  /**
+   * Carga el carrito desde el locarStorage
+   */
   cargarCarritoDesdeLocalStorage(): void {
     if (typeof localStorage !== 'undefined') {
       const juegosEnCarritoLS = localStorage.getItem('juegos-en-carrito');
@@ -53,13 +79,14 @@ export class CarritoComponent {
     }
   }
 
+  /**
+   * Guarda el estado actual del carrito en el localStorage.
+   */
   guardarCarritoEnLocalStorage(): void {
     if (typeof localStorage !== 'undefined') {
       localStorage.setItem('juegos-en-carrito', JSON.stringify(this.juegosEnCarrito));
     }
   }
 
-  logout(): void {
-    this.authService.logout();
-  }
+  
 }
