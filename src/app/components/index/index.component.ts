9b7cae0e8cbc82c7d8c14e9d6e4e48575db9b3dc
GiovannyOtarola <giovannyxdd@gmail.com>
2024-06-22 16:27:22 -0400

    documentando componente index

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -3,6 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../services/services.component';
 
+/**
+ * @description
+ * Componente principal de la aplicacion
+ * 
+ * Este componente almacena los juegos que se mostraran en la pagina principal de la aplicacion
+ */
+
 export interface Juego{
   id: String;
   categoria: String;
@@ -23,7 +30,7 @@ export interface Juego{
 
 
 export class IndexComponent {
-
+ //Lista de Juegos
   juegos: Juego[] = [
     //Juegos play 5
     {
@@ -154,6 +161,9 @@ export class IndexComponent {
     this.juegosFiltrados = this.juegos;
   }
 
+/**
+ * Inicializa el componente,estableciendo los juegos filtrados, el estado de autenticacion y cargando el carrito desde el localStorage
+ */
   ngOnInit(): void {
     this.juegosFiltrados = this.juegos;
 
@@ -168,6 +178,13 @@ export class IndexComponent {
     }
   }
 
+/**
+ * Filtra la lista de juegos segun la categoria proporcionada
+ * 
+ * @param {String} categoria  -la categoria por la cual se debe filtrar. Si es 'todos', se muestran todos los juegos
+ * @returns {void} - no retorna ningun valor
+ * 
+ */
   filtrarJuegos(categoria: string): void {
     if (categoria !== 'todos') {
       this.juegosFiltrados = this.juegos.filter(juego => juego.categoria === categoria);
@@ -177,6 +194,12 @@ export class IndexComponent {
       this.tituloPrincipal = "Todos los Juegos";
     }
   }
+
+  /**
+   * Agrega un juego al carrito. si ya esta en el carrito incrementa la cantidad.
+   * 
+   * @param {Juego} juego - El juego que se agrega al carrito.
+   */
 
   agregarAlCarrito(juego: Juego): void {
     const juegoEnCarrito = this.juegosEnCarrito.find(j => j.id === juego.id);
@@ -191,17 +214,26 @@ export class IndexComponent {
     this.guardarCarritoEnLocalStorage();
   }
 
+  /**
+   * Actualiza el numero de juegos en el carrito que se muestra en el html del componente.
+   */
   actualizarNumeroCarrito(): void {
     let nuevoNumero = this.juegosEnCarrito.reduce((acc, juego) => acc + (juego.cantidad || 0), 0);
     document.getElementById('numcarrito')!.innerText = `${nuevoNumero}`;
   }
 
+  /**
+   * Guarda el estado actual del carrito en el localStorage
+   */
   guardarCarritoEnLocalStorage(): void {
     if (typeof localStorage !== 'undefined') {
       localStorage.setItem('juegos-en-carrito', JSON.stringify(this.juegosEnCarrito));
     }
   }
 
+  /**
+   * Carga el estado del carrito desde el localStorage.
+   */
   cargarCarritoDesdeLocalStorage(): void {
     if (typeof localStorage !== 'undefined') {
       const juegosEnCarritoLS = localStorage.getItem('juegos-en-carrito');
@@ -210,6 +242,9 @@ export class IndexComponent {
     }
   }
 
+  /**
+   * Cierra la sesion del usuario.
+   */
   logout(): void {
     this.authService.logout();
   }
