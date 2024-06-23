faf176c2329304d9d3f0221aef06ddc0b394745c
GiovannyOtarola <giovannyxdd@gmail.com>
2024-06-23 12:34:50 -0400

    optimizando imagenes y documentando pruebas unitarias

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -5,10 +5,19 @@ import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+/**
+ * @description
+ * Pruebas unitarias para el componente login.
+ * 
+ * Conjunto de pruebas para la validacion del formulario de inicio de sesion.
+ */
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-  let router: Router;
+  
+  /**
+   * Configuracion inicial para el conjunto de pruebas.
+   */
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LoginComponent,CommonModule,
@@ -22,10 +31,17 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
+  /**
+   * Verifica que el componente se haya creado correctamente.
+   */
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+  /**
+   * Verifica que el formulario de inicio de sesion se inicialice correctamente.
+   * Se asegura que los campos email y password esten definidos.
+   */
   it('LoginForm inicia Correctamente',() => {
     expect(component.loginForm).toBeDefined();
     expect(component.loginForm.get('emal')).toBeDefined();
